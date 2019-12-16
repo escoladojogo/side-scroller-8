3ac7a50a146ed3dc972417953ae6964b518df0fc
lucas-machado <lucasmach@gmail.com>
2019-12-16 00:51:37 -0300

    base project structure & fox animated

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public SpriteRenderer renderer;
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+    public float runBoost = 500f;
+    public float jumpBoost = 400f;
+
+    float horizontalMove;
+    bool jump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+
+        if (horizontalMove < 0)
+        {
+            renderer.flipX = true;
+        }
+        else if (horizontalMove > 0)
+        {
+            renderer.flipX = false;
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector3(horizontalMove * Time.fixedDeltaTime * runBoost, rigidBody.velocity.y, 0);
+        if (jump)
+        {
+            rigidBody.AddForce(new Vector2(0, jumpBoost));
+            jump = false;
+        }
+
+    }
+}
