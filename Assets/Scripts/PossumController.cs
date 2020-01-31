a9c8a1d7fbaec22d79e500b86a8734853180fc34
lucas-machado <lucasmach@gmail.com>
2020-01-30 22:27:43 -0300

    final state class 4

---
+++ Assets/Scripts/PossumController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossumController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(-5, rigidBody.velocity.y);
+    }
+}
