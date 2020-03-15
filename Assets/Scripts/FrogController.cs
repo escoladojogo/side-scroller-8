227eb09bce93450ae0f05c6bf94e59d57e7affb0
lucas-machado <lucasmach@gmail.com>
2020-03-15 17:00:04 -0300

    frog jump with conditional logic

---
+++ Assets/Scripts/FrogController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController : MonoBehaviour
+{
+    public Rigidbody2D frogBody;
+
+    float secondsPassed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        secondsPassed += Time.deltaTime;
+
+        if (secondsPassed >= 2.0f)
+        {
+            frogBody.AddForce(new Vector2(0, 400));
+            secondsPassed = 0;
+        }
+    }
+}
